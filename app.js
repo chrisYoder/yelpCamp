ce37ddb49619bacc429bce81bf266276a2a55241
EC2 Default User <ec2-user@ip-172-31-43-120.us-east-2.compute.internal>
2019-04-22 15:16:50 +0000

    trying to connect mongo

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser');
 const campgrounds = [
   { name: 'campground1', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
   { name: 'campground2', image: 'https://farm1.staticflickr.com/82/225912054_690e32830d.jpg' },
-  { name: 'campground3', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg' }
+  { name: 'campground3', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg' },
+  { name: 'campground1', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
+  { name: 'campground2', image: 'https://farm1.staticflickr.com/82/225912054_690e32830d.jpg' },
+  { name: 'campground3', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg' },
+  { name: 'campground1', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
+  { name: 'campground2', image: 'https://farm1.staticflickr.com/82/225912054_690e32830d.jpg' },
+  { name: 'campground3', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg' },
         ];
 
 app.set('view engine', 'ejs');
---
+++ notes/layout.md
@@ -0,0 +1,35 @@
+# Yelp Camp
+
+### starting out
+* Add Landing Page
+* Add Campgrounds page that lists all campgrounds
+
+Each campground includes
+* name
+* image
+
+campground object:
+
+```
+[
+    {name: nameOfCampground, image: imageOfCampground} 
+]
+```
+
+### layout and basic styling
+* create header and footer partials
+* add in bootstrap
+
+### Creating new campgrounds
+* set new campground post route
+* add in body-parser
+* setup route to show form
+* add basic unstyled form
+
+### Style the campgrounds page
+* add a better header/title
+* make campgrounds display in a grid
+
+### style the navbar and form
+* Add navbar to all templates
+* style the new campground form
